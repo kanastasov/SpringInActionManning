2c5a28cfee3487ef3c86608232c75780e4fff386
kanastasov <kirilanastasoff@gmail.com>
2022-12-04 10:53:33 +0400

    fix issue with lombok

--- taco-cloud/src/main/java/com/tacocloud/kirilanastasoff/tacocloud/model/TacoOrder.java
+++ taco-cloud/src/main/java/com/tacocloud/kirilanastasoff/tacocloud/model/TacoOrder.java
@@ -19,4 +19,91 @@ public class TacoOrder {
 	public void addTaco(Taco taco) {
 		this.tacos.add(taco);
 	}
+
+	public TacoOrder(String deliveryName, String deliveryStreet, String deliveryCity, String deliveryState,
+			String deliveryZip, String ccNumber, String ccExpiration, String ccCVV, List<Taco> tacos) {
+		super();
+		this.deliveryName = deliveryName;
+		this.deliveryStreet = deliveryStreet;
+		this.deliveryCity = deliveryCity;
+		this.deliveryState = deliveryState;
+		this.deliveryZip = deliveryZip;
+		this.ccNumber = ccNumber;
+		this.ccExpiration = ccExpiration;
+		this.ccCVV = ccCVV;
+		this.tacos = tacos;
+	}
+
+	public String getDeliveryName() {
+		return deliveryName;
+	}
+
+	public void setDeliveryName(String deliveryName) {
+		this.deliveryName = deliveryName;
+	}
+
+	public String getDeliveryStreet() {
+		return deliveryStreet;
+	}
+
+	public void setDeliveryStreet(String deliveryStreet) {
+		this.deliveryStreet = deliveryStreet;
+	}
+
+	public String getDeliveryCity() {
+		return deliveryCity;
+	}
+
+	public void setDeliveryCity(String deliveryCity) {
+		this.deliveryCity = deliveryCity;
+	}
+
+	public String getDeliveryState() {
+		return deliveryState;
+	}
+
+	public void setDeliveryState(String deliveryState) {
+		this.deliveryState = deliveryState;
+	}
+
+	public String getDeliveryZip() {
+		return deliveryZip;
+	}
+
+	public void setDeliveryZip(String deliveryZip) {
+		this.deliveryZip = deliveryZip;
+	}
+
+	public String getCcNumber() {
+		return ccNumber;
+	}
+
+	public void setCcNumber(String ccNumber) {
+		this.ccNumber = ccNumber;
+	}
+
+	public String getCcExpiration() {
+		return ccExpiration;
+	}
+
+	public void setCcExpiration(String ccExpiration) {
+		this.ccExpiration = ccExpiration;
+	}
+
+	public String getCcCVV() {
+		return ccCVV;
+	}
+
+	public void setCcCVV(String ccCVV) {
+		this.ccCVV = ccCVV;
+	}
+
+	public List<Taco> getTacos() {
+		return tacos;
+	}
+
+	public void setTacos(List<Taco> tacos) {
+		this.tacos = tacos;
+	}
+
 }
