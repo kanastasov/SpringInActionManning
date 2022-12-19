5777de99ff7c37d423002ac97c4353febcfd5d80
kanastasov <kirilanastasoff@gmail.com>
2022-12-19 14:39:31 +0400

    consuming rest

--- taco-cloud/src/main/java/com/tacocloud/kirilanastasoff/tacocloud/controller/RegistrationController.java
+++ taco-cloud/src/main/java/com/tacocloud/kirilanastasoff/tacocloud/controller/RegistrationController.java
@@ -1,12 +1,15 @@
 package com.tacocloud.kirilanastasoff.tacocloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
+import com.tacocloud.kirilanastasoff.tacocloud.model.Ingredient;
 import com.tacocloud.kirilanastasoff.tacocloud.model.RegistrationForm;
 import com.tacocloud.kirilanastasoff.tacocloud.repository.UserRepository;
 
@@ -15,6 +18,8 @@ import com.tacocloud.kirilanastasoff.tacocloud.repository.UserRepository;
 public class RegistrationController {
 //	@Autowired
 //	private UserRepository userRepo;
+	
+	RestTemplate rest = new RestTemplate();
 	private PasswordEncoder passwordEncoder;
 
 //	@Autowired
@@ -44,4 +49,13 @@ public class RegistrationController {
 //		userRepo.save(form.toUser(passwordEncoder));
 		return "redirect:/login";
 	}
+	
+	public Ingredient getIngredientById(String ingredientId) {
+		ResponseEntity<Ingredient> responseEntity =
+		rest.getForEntity("http://localhost:8080/ingredients/{id}",
+		Ingredient.class, ingredientId);
+//		log.info("Fetched time: " +
+//		responseEntity.getHeaders().getDate());
+		return responseEntity.getBody();
+		}
 }
